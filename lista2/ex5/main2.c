25725c1eb011c0e98cf7a7d7a91c7fe11898d9ce
MarlonHenq <marlon@marlonhenq.dev>
2022-03-21 00:18:49 -0300

    comentários ex5 versão 2

--- lista2/ex5/main2.c
+++ lista2/ex5/main2.c
@@ -1,19 +1,20 @@
 # include <stdio.h>
 
-void criptografia(char *var){
+void criptografia(char *var){ //função para criptografar a mensagem (shift +3)
     char letra;
-    for (int i = 0; i < 30; i++){
-        if ((var[i] + 3) >= 'A' && (var[i] + 3) <= 'Z'){
+    for (int i = 0; i < 30; i++){ //loop para percorrer a mensagem
+        if ((var[i] + 3) >= 'A' && (var[i] + 3) <= 'Z'){ //Verifica se a letra +3 está entre a e Z
             letra = var[i] + 3;
             
         }
-        else{
+        else{ //Se não, volta-se para o começo do alfabeto e adiciona +3
             letra = var[i] + 3;
             letra = letra - 26;
         }
     }
 }
 
+//Main Intacta
 int main()
 {
     char text[30];
